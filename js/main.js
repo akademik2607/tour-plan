f3a9d9da6246ff92e8e9ff923e046d676055a5fb
akademik2607 <akademik2607@mail.ru>
2021-07-21 15:35:42 +0300

    fix: change classes & tyles of hotel-slider

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const swiper = new Swiper('.swiper-container', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button--next',
-    prevEl: '.swiper-button--prev',
+    nextEl: '.hotel-slider--next',
+    prevEl: '.hotel-slider--prev',
   },
   keyboard: {
     enabled: true,
